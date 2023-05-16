93c3c07bbb67e3300448d9b61427510804a2c3cb
patryk77k <patryk77.k@gmail.com>
2023-05-16 14:21:35 +0200

    add check forMatch

--- app.js
+++ app.js
@@ -48,3 +48,40 @@ const cardArray = [
     img: "images/hotdog.png",
   },
 ];
+
+cardArray.sort(() => 0.5 - Math.random());
+
+let cardChosen = [];
+
+const gridDisplay = document.querySelector("#grid");
+
+const checkMatch = () => {
+  console.log("check for a match");
+};
+
+const flipCard = (e) => {
+  const cardId = e.target.getAttribute("data-id");
+  cardChosen.push(cardArray[cardId].name);
+  console.log("clicked", cardId);
+  console.log("Card Choosen :", cardChosen);
+  e.target.setAttribute("src", cardArray[cardId].img);
+  if (cardChosen.length === 2) {
+    setTimeout(checkMatch, 500);
+  }
+};
+
+const createBoard = () => {
+  for (let i = 0; i < cardArray.length; i++) {
+    const card = document.createElement("img");
+    card.setAttribute("src", "images/blank.png");
+    card.setAttribute("data-id", i);
+    gridDisplay.appendChild(card);
+    card.addEventListener("click", flipCard);
+  }
+};
+
+createBoard();
+
+// const compare = () => {
+
+// }
